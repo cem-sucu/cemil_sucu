784095f86b4e1f800a65c03cabcc97004ab4717b
cem-sucu <cems.cems.17@gmail.com>
2023-06-06 10:08:47 +0200

    Exo 7

---
+++ AlgoPHP/Partie1/exo8.php
@@ -0,0 +1,14 @@
+<h1>Exercice 7</h1>
+
+<p>Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la forme:
+</p>
+
+<h2>Résultat</h2>
+
+<?php 
+
+
+
+
+
+?>
