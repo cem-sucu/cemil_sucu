34627935f95c8a101d7ddbbccbb460af57326742
cem-sucu <cems.cems.17@gmail.com>
2023-06-06 11:51:47 +0200

    exo11

---
+++ AlgoPHP/Partie1/exo10.php
@@ -0,0 +1,19 @@
+<h1>Exercice 10</h1>
+
+<p>
+A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 1€.
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$montantApayer = 152;
+$montantAverser = 200;
+$resteAPayer = 48;
+
+
+
+
+
+?>
