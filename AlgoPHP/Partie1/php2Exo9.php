9aa0d4c5e8627f40dbe9af6069adfc6f039dd9dd
cem-sucu <cems.cems.17@gmail.com>
2023-06-13 15:37:12 +0200

    exoç finis

---
+++ AlgoPHP/Partie1/php2Exo9.php
@@ -0,0 +1,22 @@
+<h1>Résultat</h1>
+
+<p>Créer une fonctionpersonnalisée permettant d’afficher des boutons radio avec un tableau de valeurs en paramètre("Monsieur","Madame","Mademoiselle").Exemple:afficherRadio($nomsRadio);</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$nomsRadio = array("Masculin", "Feminin", "Autre");
+
+
+function afficherRadio($nomsRadio){
+    foreach($nomsRadio as $genre){?>
+    <input type="radio" name="genre" value="<?=$genre?> ">
+        <?=$genre?><br>
+        <?php
+    }
+}
+afficherRadio($nomsRadio);
+
+
+?>
