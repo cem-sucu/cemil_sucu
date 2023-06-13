3681780c28e41ed3a95d44f2e0b00e087c88b7d8
cem-sucu <cems.cems.17@gmail.com>
2023-06-13 13:44:25 +0200

    exo5 finis

---
+++ AlgoPHP/Partie1/php2Exo5.php
@@ -0,0 +1,25 @@
+<h1>Exercice 5</h1>
+
+<p>Créer  une  fonction  personnalisée  permettant  de créer  un  formulaire  de  champs  de  texte  en précisant le nom des champs associés.Exemple:$nomsInput = array("Nom","Prénom","Ville");afficherInput($nomsInput);</p>
+
+<h2>Résultat</h2>
+
+<?php
+$nomsInput = array("Nom", "Prénom", "Ville");
+afficherInput($nomsInput);
+
+function afficherInput(array $nomsInput) {
+    ?>
+    <form>
+        <?php foreach ($nomsInput as $nom) { 
+            ?>
+            <label>
+                <?= $nom  ?>
+            </label><br>
+            <input type="text" name="<?php echo $nom; ?>"><br>
+        <?php } ?>
+        <input type="submit" value="Envoyer">
+    </form>
+    <?php
+}
+?>
