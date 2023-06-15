7460c91d97c5c7103a9d71a25c4bc69b97e695ab
cem-sucu <cems.cems.17@gmail.com>
2023-06-15 11:04:55 +0200

    exo 15 finis

--- AlgoPHP/Partie1/php2Exo15.php
+++ AlgoPHP/Partie1/php2Exo15.php
@@ -1,4 +1,32 @@
 <h1>Exercice 15</h1>
 
-<p>git config --global user.email cems.cems.17@gmail.com</p>
+<p>En  utilisant  les  ressources  de  la  page http://php.net/manual/fr/book.filter.php,  vérifier  si  une adresse e-mail a le bon format.</p>
 
+<h2>Résultat</h2>
+
+<?php
+
+    $email = "elan@elan-formation.fr";
+    $mail = "contact@elan";
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "l'adresse $email est une adresse valide"."<br>";
+    } else {
+        echo "l'adresse $email  non valide"."<br>";
+    }
+
+    if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+        echo "l'adresse $mail est une adresse valide"."<br>";
+    } else {
+        echo "l'adresse $mail  non valide"."<br>";
+    }
+
+
+
+
+
+
+
+
+
+?>
